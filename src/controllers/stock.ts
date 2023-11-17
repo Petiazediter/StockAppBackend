f57a580ee910d016b0c24a6d80f67f8c4951b261
Peti <49650243+Petiazediter@users.noreply.github.com>
2023-11-17 06:59:02 +0100

    feat: handle non-existing recorded prices

--- src/controllers/stock.ts
+++ src/controllers/stock.ts
@@ -1,10 +1,24 @@
 import express from "express"
 import axios from 'axios'
 import ensureValidSymbol from "../utils/ensureValidSymbol";
+import server from "../server";
 
 
 const getStockBySymbol = async ( req: express.Request, res: express.Response ) => {
     const symbol = req.params.symbol;
+
+    const stock = server.dbInstance.stock.findUnique({
+        where: {
+            symbol
+        }
+    })
+
+    if ( !stock ) {
+        return res.status(404)
+            .json({
+                error: 'Couldn\'t find recorded prices for this stock. Please use PUT /stock/:symbol to record prices.'
+            })
+    }
 
     try {
         await ensureValidSymbol(symbol)
